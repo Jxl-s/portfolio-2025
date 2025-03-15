7eab803090fe52e733424dd8b30a113d575e0b21
Jia <77560326+Jxl-s@users.noreply.github.com>
2025-03-14 22:05:48 -0400

    add black screen lol

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="w-dvw h-dvh flex items-center justify-center">
+    <div class="animate-pulse w-full text-center">
+        <span class="text-4xl font-bold block">Portfolio 2025</span>
+        <span class="text-lg opacity-50 block">Coming Soon ...</span>
+    </div>
+</h1>
