4981bc2e4b73951838102e41e7beaaaeaefd2e14
Jia <77560326+Jxl-s@users.noreply.github.com>
2025-03-16 16:48:17 -0400

    add education data

---
+++ src/lib/data/education.ts
@@ -0,0 +1,23 @@
+interface Education {
+	school: string;
+	degree: string;
+	graduation: string;
+	grade: string;
+}
+
+const education: Education[] = [
+	{
+		school: 'Concordia University',
+		degree: 'Bachelor of Science in Computer Science',
+		graduation: 'Expected: May 2026',
+		grade: 'GPA: 4.3/4.3'
+	},
+	{
+		school: 'Vanier College',
+		degree: 'Technical DEC in Computer Science Technology',
+		graduation: 'May 2024',
+		grade: 'R-Score: 35.0'
+	}
+];
+
+export default education;
