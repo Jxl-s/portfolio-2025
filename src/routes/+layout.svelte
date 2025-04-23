700da184258ac4adb0aec08501fa046267a6360c
Jia <77560326+Jxl-s@users.noreply.github.com>
2025-04-22 20:05:40 -0400

    update index header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,14 +3,4 @@
 	let { children } = $props();
 </script>
 
-<svelte:head>
-	<title>Jia Xuan Li</title>
-	<meta charset="utf-8" />
-	<meta name="description" content="Jia's personal website" />
-	<meta name="author" content="Jia Xuan Li" />
-
-	<!-- Make crawlable later by search engines -->
-	<meta name="robots" content="noindex, nofollow" />
-</svelte:head>
-
 {@render children()}
